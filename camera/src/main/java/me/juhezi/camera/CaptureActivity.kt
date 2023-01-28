20c813170446e1c6bffde15dddd0bb099e5b486e
Juhezi <juhezix@163.com>
2023-01-28 21:06:37 +0800

    add camera module

---
+++ camera/src/main/java/me/juhezi/camera/CaptureActivity.kt
@@ -0,0 +1,45 @@
+package me.juhezi.camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.common.util.concurrent.ListenableFuture
+import me.juhezi.camera.databinding.ActivityCaptureBinding
+
+class CaptureActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCaptureBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCaptureBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+//        设置为 textureView
+//        binding.previewView.implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+
+        // 设置缩放类型
+        binding.previewView.scaleType = PreviewView.ScaleType.FIT_CENTER
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview: Preview = Preview.Builder().build()
+        val cameraSelector: CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+}
